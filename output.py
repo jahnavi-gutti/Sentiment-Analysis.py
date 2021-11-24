3775406887aa102dd41da3e072429503a2db2001
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-11-24 21:39:40 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,65 @@
+Epoch 1/30
+49/49 [==============================] - 3s 44ms/step - loss: 0.6179 - accuracy: 0.9813 - val_loss: 0.5234 - val_accuracy: 1.0000
+Epoch 2/30
+49/49 [==============================] - 2s 41ms/step - loss: 0.4005 - accuracy: 1.0000 - val_loss: 0.2644 - val_accuracy: 1.0000
+Epoch 3/30
+49/49 [==============================] - 2s 41ms/step - loss: 0.1650 - accuracy: 1.0000 - val_loss: 0.0880 - val_accuracy: 1.0000
+Epoch 4/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0559 - accuracy: 1.0000 - val_loss: 0.0334 - val_accuracy: 1.0000
+Epoch 5/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0240 - accuracy: 1.0000 - val_loss: 0.0167 - val_accuracy: 1.0000
+Epoch 6/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0131 - accuracy: 1.0000 - val_loss: 0.0100 - val_accuracy: 1.0000
+Epoch 7/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0082 - accuracy: 1.0000 - val_loss: 0.0066 - val_accuracy: 1.0000
+Epoch 8/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0057 - accuracy: 1.0000 - val_loss: 0.0047 - val_accuracy: 1.0000
+Epoch 9/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0041 - accuracy: 1.0000 - val_loss: 0.0035 - val_accuracy: 1.0000
+Epoch 10/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0032 - accuracy: 1.0000 - val_loss: 0.0027 - val_accuracy: 1.0000
+Epoch 11/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0025 - accuracy: 1.0000 - val_loss: 0.0022 - val_accuracy: 1.0000
+Epoch 12/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0020 - accuracy: 1.0000 - val_loss: 0.0018 - val_accuracy: 1.0000
+Epoch 13/30
+49/49 [==============================] - 2s 40ms/step - loss: 0.0017 - accuracy: 1.0000 - val_loss: 0.0015 - val_accuracy: 1.0000
+Epoch 14/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0014 - accuracy: 1.0000 - val_loss: 0.0013 - val_accuracy: 1.0000
+Epoch 15/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0012 - accuracy: 1.0000 - val_loss: 0.0011 - val_accuracy: 1.0000
+Epoch 16/30
+49/49 [==============================] - 2s 39ms/step - loss: 0.0010 - accuracy: 1.0000 - val_loss: 9.3706e-04 - val_accuracy: 1.0000
+Epoch 17/30
+49/49 [==============================] - 2s 41ms/step - loss: 8.8598e-04 - accuracy: 1.0000 - val_loss: 8.1851e-04 - val_accuracy: 1.0000
+Epoch 18/30
+49/49 [==============================] - 2s 40ms/step - loss: 7.7708e-04 - accuracy: 1.0000 - val_loss: 7.2078e-04 - val_accuracy: 1.0000
+Epoch 19/30
+49/49 [==============================] - 2s 40ms/step - loss: 6.8678e-04 - accuracy: 1.0000 - val_loss: 6.3927e-04 - val_accuracy: 1.0000
+Epoch 20/30
+49/49 [==============================] - 2s 40ms/step - loss: 6.1106e-04 - accuracy: 1.0000 - val_loss: 5.7054e-04 - val_accuracy: 1.0000
+Epoch 21/30
+49/49 [==============================] - 2s 40ms/step - loss: 5.4694e-04 - accuracy: 1.0000 - val_loss: 5.1205e-04 - val_accuracy: 1.0000
+Epoch 22/30
+49/49 [==============================] - 2s 40ms/step - loss: 4.9215e-04 - accuracy: 1.0000 - val_loss: 4.6192e-04 - val_accuracy: 1.0000
+Epoch 23/30
+49/49 [==============================] - 2s 40ms/step - loss: 4.4498e-04 - accuracy: 1.0000 - val_loss: 4.1855e-04 - val_accuracy: 1.0000
+Epoch 24/30
+49/49 [==============================] - 2s 39ms/step - loss: 4.0407e-04 - accuracy: 1.0000 - val_loss: 3.8083e-04 - val_accuracy: 1.0000
+Epoch 25/30
+49/49 [==============================] - 2s 39ms/step - loss: 3.6836e-04 - accuracy: 1.0000 - val_loss: 3.4781e-04 - val_accuracy: 1.0000
+Epoch 26/30
+49/49 [==============================] - 2s 39ms/step - loss: 3.3701e-04 - accuracy: 1.0000 - val_loss: 3.1873e-04 - val_accuracy: 1.0000
+Epoch 27/30
+49/49 [==============================] - 2s 39ms/step - loss: 3.0934e-04 - accuracy: 1.0000 - val_loss: 2.9300e-04 - val_accuracy: 1.0000
+Epoch 28/30
+49/49 [==============================] - 2s 39ms/step - loss: 2.8480e-04 - accuracy: 1.0000 - val_loss: 2.7013e-04 - val_accuracy: 1.0000
+Epoch 29/30
+49/49 [==============================] - 2s 39ms/step - loss: 2.6293e-04 - accuracy: 1.0000 - val_loss: 2.4970e-04 - val_accuracy: 1.0000
+Epoch 30/30
+49/49 [==============================] - 2s 38ms/step - loss: 2.4336e-04 - accuracy: 1.0000 - val_loss: 2.3138e-04 - val_accuracy: 1.0000
+782/782 [==============================] - 1s 2ms/step - loss: 2.3138e-04 - accuracy: 1.0000
+Reviews      <START i saw this movie and i do like horror m...
+Sentiment                                             negative
+Name: 432, dtype: object
+negative sentiment
